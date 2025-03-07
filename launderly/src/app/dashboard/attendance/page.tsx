d2fb93d9be422725466ff91e0e162644e8c3e060
Allysa <nuaysa@gmail.com>
2025-03-07 18:08:45 +0700

    run build

--- launderly/src/app/dashboard/attendance/page.tsx
+++ launderly/src/app/dashboard/attendance/page.tsx
@@ -2,8 +2,8 @@ import AttendanceReportTableForOutlet from "@/components/feat-3/attendance/atten
 
 export default function AttendanceReportPage() {
   return (
-    <div className="h-full w-full flex flex-col gap-10">
-      <h1 className="text-2xl md:text-3xl font-bold mb-6 text-gray-800">Outlet Transaction Analysis</h1>
+    <div className="min-h-screen w-full max-sm:min-w-screen flex flex-col justify-center items-center">
+      <h1 className="text-2xl md:text-3xl font-bold my-3 text-gray-800">Attendance Report</h1>
       <AttendanceReportTableForOutlet />
     </div>
   );
